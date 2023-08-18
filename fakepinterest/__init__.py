3bf041021baf386b2cc8b5953165d6446fdbbba3
Pedro Bittencourt <pedrovbittencourt@gmail.com>
2023-08-18 17:32:48 -0300

    Criando Postgre Automático

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
@@ -13,5 +14,14 @@ database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "homepage"
+
+from fakepinterest import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
 
 from fakepinterest import routes
