ffbc76aaa71e4d62427904b34cca7c3b8e52d7ab
Pedro Bittencourt <pedrovbittencourt@gmail.com>
2023-08-20 13:35:08 -0300

    Lógica para database local/online

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,7 +5,11 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+if os.getenv("DATABASE_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+
 app.config["SECRET_KEY"] = "56129debfb2b9ecc5824842d47101afd"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
